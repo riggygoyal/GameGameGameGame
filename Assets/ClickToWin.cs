27c03132c0bf90b7c3d20a6506d1e88c77d4d9e5
Rigved Goyal <riggygoyal@lawn-143-215-126-195.lawn.gatech.edu>
2019-09-28 23:16:50 -0400

    Created enemy object and programmed it to move

---
+++ Assets/ClickToWin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToWin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            PersistentDataManager.run.GameWon();
+        }
+    }
+}
---
+++ Assets/RandomMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MinigameBehaviour
+{
+    Vector3 pos = new Vector3(0.15f, 0.15f);
+    float wait;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+
+    }
+
+    protected override void OnStateEnter()
+    {
+        
+    }
+
+    protected override void OnStateExit()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (wait >= 3.5)
+        {
+            if (transform.position.x <= 15 & transform.position.y <= 15)
+            {
+                transform.position += pos;
+            }
+        }
+        wait += Time.deltaTime;
+    }
+}
